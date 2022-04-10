58f02a991129a4e2236002df0067773f565ce9ed
sshacker <shivamsonkar260@gmail.com>
2022-04-11 04:42:18 +0530

    rendering tesMe page

--- MyFirstGrailsProject/grails-app/controllers/myfirstgrailsproject/HomeController.groovy
+++ MyFirstGrailsProject/grails-app/controllers/myfirstgrailsproject/HomeController.groovy
@@ -3,9 +3,14 @@ package myfirstgrailsproject
 class HomeController {
 
     HomeService homeService
+
     def index() {
         String name = homeService.serviceMethod();
         render(view: 'index', model: [name: name, age: 23])
+    }
+
+    def testMe () {
+        render(view: 'testMe', model: [firstName:"Shivam", lastName:"Sonkar"])
     }
     // def homepage() { }
 }
---
+++ MyFirstGrailsProject/grails-app/views/home/testMe.gsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: shivam
+  Date: 11/04/22
+  Time: 4:39 AM
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+    hello, ${firstName} ${lastName} how are you.
+</body>
+</html>
