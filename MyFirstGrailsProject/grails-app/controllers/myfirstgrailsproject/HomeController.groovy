68b2468bcf7ff61fdcef06257a7df84bbc817cf3
sshacker <shivamsonkar260@gmail.com>
2022-04-11 05:07:43 +0530

    adding animal list

--- MyFirstGrailsProject/grails-app/controllers/myfirstgrailsproject/HomeController.groovy
+++ MyFirstGrailsProject/grails-app/controllers/myfirstgrailsproject/HomeController.groovy
@@ -1,8 +1,11 @@
 package myfirstgrailsproject
 
 class HomeController {
-
     HomeService homeService
+
+    def homepage(){
+
+    }
 
     def index() {
         String name = homeService.serviceMethod();
@@ -12,5 +15,9 @@ class HomeController {
     def testMe () {
         render(view: 'testMe', model: [firstName:"Shivam", lastName:"Sonkar"])
     }
-    // def homepage() { }
+
+    def animal(){
+        List animalList = ["Lion", "Tiger", "Deer", "Panther", "Jackal"]
+        render(view: "animal", model: [animalListKey: animalList])
+    }
 }
---
+++ MyFirstGrailsProject/grails-app/views/home/animal.gsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: shivam
+  Date: 11/04/22
+  Time: 4:57 AM
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+    <g:each in="${animalListKey}" var="animalList">
+        ${animalList}
+    </g:each>
+</body>
+</html>
